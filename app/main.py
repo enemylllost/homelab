7501d3d3e85c352fbe7936eea1507533a21ebfae
enemylllost <dereskovivan2@gmail.com>
2026-09-16 20:50:52 +0300

    html and css for root

--- app/main.py
+++ app/main.py
@@ -1,15 +1,24 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 
 from app.schemas import RootSchema
 from app.routers import health, menu
 
 app = FastAPI()
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+templates = Jinja2Templates(directory="app/templates")
+
 app.include_router(menu.router)
 app.include_router(health.router)
 
-@app.get("/", response_model=RootSchema)
-async def root():
-    return RootSchema(
-        message="hello. go to /menu"
+
+@app.get("/", response_class=HTMLResponse)
+async def root(request: Request):
+    return templates.TemplateResponse(
+        request=request,
+        name="index.html",
+        context={"message": "hello. go to /menu"},
     )
